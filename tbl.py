98157b2551d5572b370350854e852f7ac91b024d
Ian Fisher <ianfisher45@gmail.com>
2017-04-13 23:08:54 -0400

    Improve performance of the TBL tagger
    The training portion of the TBL tagger was improved so that it can be
    run in 10-20 minutes on a corpus of more than a million words.

--- tbl.py
+++ tbl.py
@@ -4,31 +4,19 @@ from operator import itemgetter
 from functools import lru_cache
 from itertools import product
 
+import nltk
+from nltk.corpus import brown
+
 class Tagger:
     tagset = ()
 
     def __init__(self, tagged_corpus):
         """Initialize a tagger on the training corpus. This may take a while."""
-        self.known_tags = {}
-        counted = Counter(tagged_corpus)
-        for word, _ in tagged_corpus:
-            if word not in self.known_tags:
-                tag_counts = ((tag, count) for (w, tag), count in counted.items() if w == word)
-                tag, _ = max(tag_counts, key=itemgetter(1))
-                self.known_tags[word] = tag
-        self.transforms = []
-        score_threshold = len(tagged_corpus) * 0.01
-        training_corpus = self.apply_ml_tag(word for word, _ in tagged_corpus)
-        # generate an immutable set of all possible transforms
-        all_transforms = frozenset(Transform(*trp) for trp in product(self.tagset, repeat=3) 
-                                                           if trp[0] != trp[1])
-        while True:
-            transform, score = self.best_transform(training_corpus, tagged_corpus, all_transforms)
-            if transform is not None and score < score_threshold:
-                self.transforms.append(transform)
-                training_corpus = transform(training_corpus)
-            else:
-                break
+        cfd = nltk.ConditionalFreqDist(tagged_corpus)
+        self.known_tags = dict((word, freq.max()) for word, freq in cfd.items())
+        my_corpus = self.tag_most_likely(word for word, _ in tagged_corpus)
+        self.transforms = [t for t in self.most_common_transforms(my_corpus, tagged_corpus)
+                                   if self.score_transform(t, my_corpus, tagged_corpus) > 0]
 
     def tag(self, text):
         """Add part-of-speech tags to the text. The argument can be a list of words or a string. If
@@ -37,16 +25,16 @@ class Tagger:
         """
         if isinstance(text, str):
             text = self.process_string(text)
-        tagged_text = self.apply_ml_tag(text)
+        tagged_text = self.tag_most_likely(text)
         for transform in self.transforms:
             tagged_text = transform(tagged_text)
         return tagged_text
 
-    def apply_ml_tag(self, text):
-        """Apply the most likely tag to each word in a text."""
-        return [(word, self.most_likely_tag(word)) for word in text]
+    def tag_most_likely(self, text):
+        """Add the most likely part-of-speech tags to the text based solely on POS frequency."""
+        return [(word, self.tag_word(word)) for word in text]
 
-    def most_likely_tag(self, word):
+    def tag_word(self, word):
         """Return the most likely POS tag for a word. If the word is not present in the tagger's 
            corpus, 'UNK' is returned.
         """
@@ -59,22 +47,30 @@ class Tagger:
         return len(list(filter(pred, zip(text1, text2))))
 
     @classmethod
-    def best_transform(cls, tagged_text, correct_tags, all_transforms):
-        """Given a tagged text and its correct tags, return the pair (transform, count), where
-           transform is the best transformation to apply from the all_transforms set and count is 
-           the number of changes it makes.
-        """
-        best_so_far = (None, cls.compare_texts(correct_tags, tagged_text))
-        for transform in all_transforms:
-            transformed = transform(tagged_text)
-            score = cls.compare_texts(correct_tags, transformed)
-            if score < best_so_far[1]:
-                best_so_far = (transform, score)
-        return best_so_far
+    def all_transforms(cls, my_tagged_text, correct_tagged_text):
+        for i, (_, tag) in enumerate(correct_tagged_text[1:], start=1):
+            my_tag = my_tagged_text[i][1]
+            if my_tag != tag:
+                yield Transform(my_tag, tag, my_tagged_text[i-1][1])
+
+    @classmethod
+    def most_common_transforms(cls, my_tagged_text, correct_tagged_text, threshold=0.00005):
+        c = Counter(cls.all_transforms(my_tagged_text, correct_tagged_text))
+        threshold_count = len(correct_tagged_text) * threshold
+        for transform, count in c.items():
+            if count >= threshold_count:
+                yield transform
+
+    @classmethod
+    def score_transform(cls, transform, my_tagged_text, correct_tagged_text):
+        original_score = cls.compare_texts(my_tagged_text, correct_tagged_text)
+        new_score = cls.compare_texts(transform(my_tagged_text), correct_tagged_text)
+        return original_score - new_score
 
     @staticmethod
     def process_string(s):
-        """Turn a string into a list of words, suitable to be passed to the apply_ml_tag method."""
+        """Turn a string into a list of words, suitable to be passed to the tag_most_likely method.
+        """
         # put spaces around "n't" so that it gets split
         s = s.replace("n't", " n't ")
         # split on whitespace, commas, and periods, capturing the latter two
@@ -84,11 +80,17 @@ class Tagger:
 
 
 class Transform(namedtuple('Transform', ['orig', 'new', 'before'])):
-    def mutate(self, tagged_text):
-        """Transform the text (a mutable sequence of word-tag pairs) in-place.  Note that the 
-           'before' member of the transform is checked against the transformed text, so for example 
-           NN -> VB / NN applied to NN NN NN becomes NN VB NN and not NN VB VB.
+    def __call__(self, tagged_text):
+        """Transform the text (a mutable sequence of word-tag pairs) and return the result. Note 
+           that the 'before' member of the transform is checked against the transformed text, so for
+           example NN -> VB / NN applied to NN NN NN becomes NN VB NN and not NN VB VB.
         """
+        tagged_text_copy = tagged_text[:]
+        self.mutate(tagged_text_copy)
+        return tagged_text_copy
+
+    def mutate(self, tagged_text):
+        """Same as __call__, except the text is modified in-place."""
         prev_tag = None
         for i, (word, tag) in enumerate(tagged_text):
             if prev_tag == self.before and tag == self.orig:
@@ -97,22 +99,26 @@ class Transform(namedtuple('Transform', ['orig', 'new', 'before'])):
             else:
                 prev_tag = tag
 
-    def __call__(self, tagged_text):
-        """Same as mutate, except return the result instead of modifying the argument."""
-        prev_tag = None
-        ret = []
-        for word, tag in tagged_text:
-            if prev_tag == self.before and tag == self.orig:
-                ret.append((word, self.new))
-                prev_tag = self.new
-            else:
-                ret.append((word, tag))
-                prev_tag = tag
-        return ret
-
     def __str__(self):
         return '{0.orig} -> {0.new} / {0.before} _'.format(self)
 
 def tagged_text_to_str(tagged_text):
     """Convenience function to turn a tagged text into a readable string."""
     return '  '.join('{} ({})'.format(word, tag) for word, tag in tagged_text)
+
+class BrownTagger(Tagger):
+    tagset = ('.', '(', ')', '*', '--', ',', ':', 'ABL', 'ABN', 'ABX', 'AP',
+              'AT', 'BE', 'BED', 'BEDZ', 'BEG', 'BEM', 'BEN', 'BER', 'BEZ', 'CC', 'CD',
+              'CS', 'DO', 'DOD', 'DOZ', 'DT', 'DTI', 'DTX', 'EX', 'FW', 'HV', 'HVD',
+              'HVG', 'HVN', 'IN', 'JJ', 'JJR', 'JJS', 'JJT', 'MD', 'NC', 'NN', 'NN$',
+              'NNS', 'NNS$', 'NP', 'NP$', 'NPS', 'NPS$', 'NR', 'OD', 'PN', 'PN$', 'PP$',
+              'PP$$', 'PPL', 'PPLS', 'PPO', 'PPS', 'PPSS', 'PRP', 'PRP$', 'QL', 'QLP',
+              'RB', 'RBR', 'RBT', 'RN', 'RP', 'TO', 'UH', 'VB', 'VBD', 'VBG', 'VBN',
+              'VBP', 'VBZ', 'WDT', 'WPO', 'WPS', 'WQL', 'WRB')
+
+if __name__ == '__main__':
+    corpus = list(brown.tagged_words())
+    tagger = BrownTagger(corpus)
+    print('{} total transformations'.format(len(tagger.transforms)))
+    #print(tagger.transforms)
+    print(tagged_text_to_str(tagger.tag('colorless green ideas sleep furiously')))
--- tests.py
+++ tests.py
@@ -6,6 +6,16 @@ import tbl
 class SimpleTagger(tbl.Tagger):
     tagset = ('JJ', 'DET', 'NN', 'TO', 'VB')
 
+class BrownTagger(tbl.Tagger):
+    tagset = ('.', '(', ')', '*', '--', ',', ':', 'ABL', 'ABN', 'ABX', 'AP',
+              'AT', 'BE', 'BED', 'BEDZ', 'BEG', 'BEM', 'BEN', 'BER', 'BEZ', 'CC', 'CD',
+              'CS', 'DO', 'DOD', 'DOZ', 'DT', 'DTI', 'DTX', 'EX', 'FW', 'HV', 'HVD',
+              'HVG', 'HVN', 'IN', 'JJ', 'JJR', 'JJS', 'JJT', 'MD', 'NC', 'NN', 'NN$',
+              'NNS', 'NNS$', 'NP', 'NP$', 'NPS', 'NPS$', 'NR', 'OD', 'PN', 'PN$', 'PP$',
+              'PP$$', 'PPL', 'PPLS', 'PPO', 'PPS', 'PPSS', 'PRP', 'PRP$', 'QL', 'QLP',
+              'RB', 'RBR', 'RBT', 'RN', 'RP', 'TO', 'UH', 'VB', 'VBD', 'VBG', 'VBN',
+              'VBP', 'VBZ', 'WDT', 'WPO', 'WPS', 'WQL', 'WRB')
+
 class TaggerTests(unittest.TestCase):
     def test_small(self):
         # the point of this corpus is to generate a NN -> VB / TO _ rule
@@ -13,8 +23,8 @@ class TaggerTests(unittest.TestCase):
                   ('the', 'DET'), ('race', 'NN'), ('happened', 'VB'),
                   ('the', 'DET'), ('race', 'NN'), ('continued', 'VB')]
         tagger = SimpleTagger(corpus)
-        self.assertEqual(tagger.most_likely_tag('race'), 'NN')
-        self.assertEqual(tagger.most_likely_tag('the'), 'DET')
+        self.assertEqual(tagger.tag_word('race'), 'NN')
+        self.assertEqual(tagger.tag_word('the'), 'DET')
         self.assertEqual(len(tagger.transforms), 1)
         self.assertEqual(tagger.transforms[0], tbl.Transform('NN', 'VB', 'TO'))
         self.assertEqual(tagger.tag('the race'), [('the', 'DET'), ('race', 'NN')])
