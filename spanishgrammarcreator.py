dd0436c3b7450b16a335f91f668c89971c4148b2
TruckFeet <henrymohr7@gmail.com>
2017-04-25 12:17:23 -0400

    Made it construct a complete grammar
    Uncommented some things and changed others so that it uses all of cess_esp, and also writes to file.

--- spanishgrammarcreator.py
+++ spanishgrammarcreator.py
@@ -12,9 +12,9 @@ def MakeSpanishGrammar(filelist):
 			for newRule in rules: #gets the grammar rules used in that sentence and adds them to the grammar
 				if newRule not in grammar: #makes sure it doesn't double-count
 					grammar += newRule
-	#target = open('spanishgrammar.py','w')
-	#target.write('spanishGrammar = ' + str(grammar))
-	#target.close()
+	target = open('spanishgrammar.py','w')
+	target.write('spanishGrammar = ' + str(grammar))
+	target.close()
 	return grammar
 
 def getStructure(tree): #makes a list of all the rules used in the sentence
@@ -36,4 +36,4 @@ def checkGrammar(grammar): #mostly for debugging
 	for key in grammar:
 		print(key)
 
-print(MakeSpanishGrammar([u't6-0.tbf', u't6-1.tbf', u't6-2.tbf', u't6-3.tbf', u't6-4.tbf', u't6-5.tbf', u't6-6.tbf'])) #the grammar is very long. be careful.
+print(MakeSpanishGrammar(cess_esp.fileids()) #the grammar is very long. be careful.
