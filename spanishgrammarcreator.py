23111fa985c1df4594d7d790559929e2a11186b9
TruckFeet <henrymohr7@gmail.com>
2017-04-25 10:01:50 -0400

    Changed format of output
    Changed from a grammar to a list of strings, hopefully now compatible with the parser.

--- spanishgrammarcreator.py
+++ spanishgrammarcreator.py
@@ -5,40 +5,35 @@ from nltk.corpus import cess_esp
 from nltk.tree import Tree
 
 def MakeSpanishGrammar(filelist):
-	grammar = {}
+	grammar = []
 	for fileid in filelist: 
 		for tree in cess_esp.parsed_sents(fileid): #for each sentence in the corpus
 			rules = getStructure(tree)
 			for newRule in rules: #gets the grammar rules used in that sentence and adds them to the grammar
-				if newRule[0] in grammar: #makes sure it doesn't double-count
-					rule = grammar[newRule[0]]
-					if newRule[1] not in rule:
-						rule += [newRule[1]]
-						grammar[newRule[0]] = rule
-				else:
-					grammar[newRule[0]] = [newRule[1]]
-	target = open('spanishgrammar.py','w')
-	target.write('spanishGrammar = ' + str(grammar))
-	target.close()
+				if newRule not in grammar: #makes sure it doesn't double-count
+					grammar += newRule
+	#target = open('spanishgrammar.py','w')
+	#target.write('spanishGrammar = ' + str(grammar))
+	#target.close()
 	return grammar
 
 def getStructure(tree): #makes a list of all the rules used in the sentence
 	if not isinstance(tree,Tree):
 		return []
 	rules = []
-	currentRule = [tree.label(),[]] #each rule is a list of a key-string and a value-list, to be put into the grammar eventually
+	currentRule = [tree.label() + " ->"] #each rule is a list of a key-string and a value-list, to be put into the grammar eventually
 	for child in tree:
 		if isinstance(child, Tree):
-			currentRule[1] += [child.label()] #adds the child to the value-list
+			currentRule[0] += " " + child.label() #adds the child to the value-list
 			newRules = getStructure(child) #checks each of the children for the rules they contain
 			rules += newRules
 		else:
-			currentRule[1] += [child]
+			currentRule[0] += " " + child
 	rules += [currentRule]
 	return rules
 
 def checkGrammar(grammar): #mostly for debugging
 	for key in grammar:
-		print key
+		print(key)
 
-MakeSpanishGrammar(cess_esp.fileids()) #the grammar is very long. be careful.
+print(MakeSpanishGrammar([u't6-0.tbf', u't6-1.tbf', u't6-2.tbf', u't6-3.tbf', u't6-4.tbf', u't6-5.tbf', u't6-6.tbf'])) #the grammar is very long. be careful.
