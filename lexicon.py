ecb4e77719d463e22778b6464aa635f276580d1e
Henry Mohr <hmohr@haverford.edu>
2017-05-09 14:14:55 -0400

    added initial version of lexical transfer, made test grammar work with test sentence

--- lexicon.py
+++ lexicon.py
@@ -1,2 +1,12 @@
+import nltk
+
+from nltk.tree import Tree
+
+from spanishdictionary import spanishDictionary
+
 def lexical_transfer(tree):
-    pass
+	sentence = ""
+	for child in tree:
+		if not isinstance(child, Tree): #if it's a string
+			sentence += spanishDictionary[child][0] #uses the first word that is a synonym
+	return sentence
