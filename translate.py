46a3c397e798fcaef36f29b9ff4c60b7f72d16ba
Ian Fisher <ianfisher45@gmail.com>
2017-05-11 16:24:14 -0400

    Change translator to handle failed parse gracefully, and update grammar

--- translate.py
+++ translate.py
@@ -5,17 +5,20 @@
 from tbl import Tagger
 from lexicon import lexical_transfer
 from syntax import syntactic_transfer
-from cfg import CFGrammar
+from cfg import CFGrammar, Tree
 
 def translate(sentence):
     tagger = Tagger.load('cess.tag')
     g = CFGrammar.from_file('grammar.txt')
     tags = tagger.tag(sentence)
-    print "Tags: " + str(tags)
+    #print "Tags: " + str(tags)
     trees = g.parse(tags)
-    print 'Tree: ' + str(trees)
-    transferred = [syntactic_transfer(tree) for tree in trees]
-    print 'Transferred: ' + str(transferred)
-    return [lexical_transfer(tree) for tree in transferred]
+    if trees:
+        #print 'Tree: ' + str(trees)
+        transferred = [syntactic_transfer(tree) for tree in trees]
+        #print 'Transferred: ' + str(transferred)
+        return [lexical_transfer(tree) for tree in transferred]
+    else:
+        return [lexical_transfer(Tree('-', tags))]
 
 print str(translate('Yo tener un perro rojo .'))
